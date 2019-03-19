727862891c481376a4574c578d1bbd6cb03f8330
amandamorato <amandamorato@live.com>
2019-03-19 19:04:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include "io.h"
-#include "config.h"
-#include "manobras.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
       int result;
-
-      le_arquivo_config("arquivoconfig.txt");
-      le_arquivo_manobras("arquivomanobras.txt");
-            
+      if (argc=3){
+            result = le_arquivo_config(argv[1]);
+            result = le_arquivo_manobras(argv[2]);
+      }
+      else{
+            printf("Quantidade de argumentos errada.\n");
+      }
       return 0;
 }
