370bf4bdb8c2f6e364904e0b28021336c3e1f5a2
amandamorato <amandamorato@live.com>
2019-03-19 19:01:56 -0300

    Update io.c

--- io.c
+++ io.c
@@ -1,79 +1,91 @@
 #include "io.h"
 
-typedef struct Lista_Veiculos{
-      struct Veiculo veiculo;
-      struct Lista_Veiculos *prox;
-} lista_veiculos;
 
 int le_arquivo_config(char* arq_config){
       char nome_veiculo;
       int tipo_veiculo;
       char eixo_veiculo;
       int x, y;
-
-      //Abertura do arquivo de Configuração inicial
       FILE *arqc = fopen(arq_config, "r");
-
-      //Verifica se o arquivo foi aberto corretamente
       if(arqc == NULL){
             printf("Erro na abertura do arquivo %s\n", arq_config);
-            return 1;
+            return 0;
       }
-
-      //lê todo o arquivo de manobras e envia para memoria
+      lista_veiculos *lista_config;
+      lista_config = cria_lista();
+      if (lista_config == NULL){
+            printf("ERRO 1\n");
+      }
       while(!feof(arqc)){
             fscanf(arqc, "%c %d %c X%dY%d\n", &nome_veiculo, &tipo_veiculo, &eixo_veiculo, &x, &y);
-
-            printf("%c %d %c X%dY%d\n",nome_veiculo, tipo_veiculo, eixo_veiculo, x, y); // teste de leitura do arquivo
+            adicionar_veiculo(&lista_config, nome_veiculo, tipo_veiculo, eixo_veiculo, x, y);
       }
+      posiciona_veiculo(&lista_config);
+
+      //imprime_lista(&lista_config);
       fclose(arqc);
-      return 0;
+      return 1;
 }
 
-//criar lista encadeada dos veículos e das manobras e depois enviar para diminuir o tempo de execução
-//posiciona_veiculo(nome_veiculo, tipo_veiculo, eixo_veiculo, x, y);
-lista_veiculos *criar_lista(){
-
-      lista_veiculos *li = (lista_veiculos*)malloc(sizeof(lista_veiculos));
-
+lista_veiculos *cria_lista(){
+      lista_veiculos *li = (lista_veiculos*) malloc (sizeof(lista_veiculos));
       if(li == NULL){
-            printf("Falha ao alocar memória.\n");
+            printf("Erro ao alocar memória.\n");
             return NULL;
       }
+      li->prox = NULL;
       return li;
 }
 
-lista_veiculos *adicionar_veiculo(lista_veiculos *li, char nome_veiculo, int tipo_veiculo, char eixo_veiculo, int x, int y){
-      
-      li->veiculo.nome_veiculo = nome_veiculo;
-      li->veiculo.tipo_veiculo = tipo_veiculo;
-      li->veiculo.eixo_veiculo = eixo_veiculo;
-      li->veiculo.x = x;
-      li->veiculo.y = y;
-      li->prox = NULL;
+void adicionar_veiculo(lista_veiculos **li, char nome_veiculo, int tipo_veiculo, char eixo_veiculo, int x, int y){
+      lista_veiculos *novo = (lista_veiculos*) malloc(sizeof(lista_veiculos));
+      if(novo == NULL){
+            printf("Erro ao alocar memória.\n");
+            exit(0);
+      }
+      novo->veiculo.nome_veiculo = nome_veiculo;
+      novo->veiculo.tipo_veiculo = tipo_veiculo;
+      novo->veiculo.eixo_veiculo = eixo_veiculo;
+      novo->veiculo.x = x;
+      novo->veiculo.y = y;
+      novo->prox = NULL;
+
+      if(*li == NULL){
+            *li = novo;
+      }
+      else{
+            lista_veiculos *aux;
+            aux = *li;
+            while (aux->prox != NULL){
+                  aux = aux->prox;
+            }
+            aux->prox = novo;
+      }
 }
+
+/*void imprime_lista(lista_veiculos **li){
+      lista_veiculos *aux;
+      aux = *li;
+      while(aux->prox!=NULL){
+            aux = aux->prox;
+            printf("%c %d %c X%dY%d\n", aux->veiculo.nome_veiculo, aux->veiculo.tipo_veiculo, aux->veiculo.eixo_veiculo, aux->veiculo.x, aux->veiculo.y);
+      }
+}*/
 
 int le_arquivo_manobras(char* arq_manobras){
       char nome_veiculo_mov;
       char eixo_mov;
       char sign;
       int posicoes_mov;
-
-      //Abertura do arquivo de MANOBRAS
       FILE *arqm = fopen(arq_manobras, "r");
-
-      //Verifica se o arquivo foi aberto corretamente
       if(arqm == NULL){
             printf("Erro na abertura do arquivo %s\n", arq_manobras);
-            return 1;
+            return 0;
       }
-
-      //lê todo o arquivo de manobras e envia para memoria
+      
       while(!feof(arqm)){
             fscanf(arqm, "%c %c %c%d\n", &nome_veiculo_mov, &eixo_mov, &sign, &posicoes_mov);
-            printf("%c %c %c%d\n", nome_veiculo_mov, eixo_mov, sign, posicoes_mov); //teste leitura de arquivo manobras
       }
-
       fclose(arqm);
-      return 0;
+      return 1;
 }
